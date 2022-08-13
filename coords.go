72a73f5f252e9ee06797d410f1b640f5a1df6898
Filip <dev.filip0s@protonmail.com>
2022-08-13 20:42:00 +0200

    Method renamed to `checkCoordsDomain`.
    Method `check` was renamed to `checkCoordsDomain` to better describe what it does.

--- coords.go
+++ coords.go
@@ -11,7 +11,8 @@ type Coords struct {
 
 // Checks if the coordinates in struct are in the 0 to cellsPerSide range.
 // Returns error in case, that one of the coordinates is outside the range, otherwise returns nil.
-func (c Coords) check() error {
+func (c Coords) checkCoordsDomain() error {
+
 	if c.row >= cellsPerSide || c.col >= cellsPerSide || c.row < 0 || c.col < 0 {
 		return fmt.Errorf("You are trying to access out of bounds cell.\nPlease use numbers between 0 and %d.\n",
 			cellsPerSide-1)
