1a6f7bb999e7b53f705106c1ddc694f6903399e8
Filip <dev.filip0s@protonmail.com>
2022-08-15 19:09:36 +0200

    Adjustments to `board.go` file
    Param `position` in the `insert` method was renamed to `pos` to be more consistent with rest of the codebase.
    
    Added `check` method to abstract both checks on coordinates before inserting.
    
    `checkCoordsDomain` moved from `coords.go` file and turned into function which takes in `pos` parameter.
    
    Added `checkEmpty` method which checks if the position is empty.

--- board.go
+++ board.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Tile string
 
@@ -47,8 +50,44 @@ func (b *Board) print() {
 	}
 }
 
-func (b *Board) insert(position Coords, player Player) {
-	b.matrix[position.row][position.col] = player.playerCharacter
+func (b *Board) insert(pos Coords, player Player) {
+	b.matrix[pos.row][pos.col] = player.playerCharacter
 }
 
-//TODO: Check if cell where player is trying to insert is empty
+// Checks validity of coordinates. Method tests if coordinates are on the playing
+// board and if the cell is empty.
+func (b *Board) check(pos Coords) error {
+	err := checkCoordsDomain(pos)
+	if err != nil {
+		return err
+	}
+
+	err = b.checkEmpty(pos)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Checks if the coordinates in struct are in the 0 to cellsPerSide-1 range.
+// Returns error in case, that one of the coordinates is outside the range, otherwise returns nil.
+func checkCoordsDomain(pos Coords) error {
+	var maximumPosition = cellsPerSide - 1
+
+	if pos.row > maximumPosition || pos.col > maximumPosition || pos.row < 0 || pos.col < 0 {
+		errOutput := fmt.Sprintf("You are trying to access out of bounds cell.\nPlease use numbers between 0 and %d.",
+			maximumPosition)
+		return errors.New(errOutput)
+	}
+	return nil
+}
+
+// Checks if coordinates passed as parameter are empty.
+func (b *Board) checkEmpty(pos Coords) error {
+	if b.matrix[pos.row][pos.col] != empty {
+		return errors.New("inserting into full cell")
+	}
+
+	return nil
+}
