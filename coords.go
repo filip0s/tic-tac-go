f7819d1527678f5a6ed25c2047b727cea2e055be
Filip <dev.filip0s@protonmail.com>
2022-08-15 19:05:50 +0200

    `checkCoordsDomain` method moved into `board.go`.
    Also fixed spelling mistake.

--- coords.go
+++ coords.go
@@ -1,22 +1,8 @@
 package main
 
-import "fmt"
-
-// Coords stores coordinations of playing board cell in which one of the players
+// Coords stores coordinates of playing board cell in which one of the players
 // try to put his marker
 type Coords struct {
 	row int
 	col int
-}
-
-// Checks if the coordinates in struct are in the 0 to cellsPerSide range.
-// Returns error in case, that one of the coordinates is outside the range, otherwise returns nil.
-func (c Coords) checkCoordsDomain() error {
-
-	if c.row >= cellsPerSide || c.col >= cellsPerSide || c.row < 0 || c.col < 0 {
-		return fmt.Errorf("You are trying to access out of bounds cell.\nPlease use numbers between 0 and %d.\n",
-			cellsPerSide-1)
-	}
-
-	return nil
 }
